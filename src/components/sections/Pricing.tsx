d39ace80bfd17b0c85958fd1f4f9ac8255a0a662
stevexero <steveanthony999@gmail.com>
2024-09-02 19:26:27 -0700

    Styled pricing header and toggle

--- src/components/sections/Pricing.tsx
+++ src/components/sections/Pricing.tsx
@@ -12,84 +12,127 @@ const Pricing = () => {
   });
   const [timeFrame, setTimeFrame] = useState('monthly');
 
-  const handleRadio = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTimeFrame(e.target.id);
-    if (e.target.id === 'monthly') {
-      setPrice({ ...price, personal: 15, family: 25 });
+  const handleToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const isYearly = e.target.checked;
+
+    setTimeFrame(isYearly ? 'yearly' : 'monthly');
+
+    if (isYearly) {
+      const personalPrice = +(price.personal * 12 * 0.55).toFixed(2);
+      const familyPrice = +(price.family * 12 * 0.55).toFixed(2);
+      const personalDiscount = +(personalPrice / 12).toFixed(2);
+      const familyDiscount = +(familyPrice / 12).toFixed(2);
+
+      setDiscount({
+        personal: personalDiscount,
+        family: familyDiscount,
+      });
+
+      setPrice({
+        ...price,
+        personal: personalPrice,
+        family: familyPrice,
+      });
     } else {
       setDiscount({
-        ...discount,
-        personal: price.personal - price.personal * 0.45,
-        family: price.family - price.family * 0.45,
+        personal: 0,
+        family: 0,
       });
       setPrice({
         ...price,
-        personal: price.personal * 12 - price.personal * 0.45 * 12,
-        family: price.family * 12 - price.family * 0.45 * 12,
+        personal: 15,
+        family: 25,
       });
     }
   };
 
   return (
-    <section id='pricing'>
-      <h2>Simple Pricing</h2>
-      <p>
-        With plans to fit your needs, our tools are an investment in your
-        journey towards personal success
-      </p>
-      <label htmlFor='time-frame'>Monthly</label>
-      <input
-        type='radio'
-        name='time-frame'
-        id='monthly'
-        defaultChecked
-        onChange={handleRadio}
-      />
-      <label htmlFor='time-frame'>Yearly (save 45%)</label>
-      <input
-        type='radio'
-        name='time-frame'
-        id='yearly'
-        onChange={handleRadio}
-      />
+    <section
+      id='pricing'
+      className='h-[50rem] w-full bg-neutral-950 bg-dot-white/[0.2] relative flex items-center justify-center'
+    >
+      <div className='absolute pointer-events-none inset-0 flex items-center justify-center bg-neutral-950 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
       <div>
-        <h3>Free</h3>
-        <h4>
-          {price.free} / {timeFrame}
-        </h4>
-        <h5></h5>
-        <ul>
-          <li></li>
-        </ul>
-        <button>Get Started</button>
-      </div>
-      <div>
-        <h3>Personal</h3>
-        <h4>
-          {price.personal} / {timeFrame}
-        </h4>
-        <h5></h5>
-        <ul>
-          <li></li>
-        </ul>
-        <button>Get Started</button>
-        {timeFrame === 'yearly' ? (
-          <>That&apos;s {discount.personal} per month!</>
-        ) : null}
-      </div>
-      <div>
-        <h3>Family</h3>
-        <h4>
-          {price.family} / {timeFrame}
-        </h4>
-        <h5></h5>
-        <ul>
-          <li></li>
-        </ul>
-        <button>Get Started</button>
-        {timeFrame === 'yearly' ? (
-          <>That&apos;s {discount.family} per month!</>
-        ) : null}
+        <h2 className='text-4xl md:text-7xl font-medium tracking-tight relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8 text-center'>
+          Simple Pricing
+        </h2>
+        <p className='text-neutral-200 max-w-md mx-auto text-center relative z-10 px-8 sm:px-0'>
+          With plans to fit your needs, our tools are an investment in your
+          journey towards personal success
+        </p>
+        <div className='flex justify-center mt-8'>
+          <form className='flex space-x-4  antialiased items-center'>
+            <p
+              className={`text-sm ${timeFrame === 'monthly' ? 'text-neutral-100' : 'text-neutral-400'} transition duration-200`}
+            >
+              Monthly
+            </p>
+            <label
+              htmlFor=':r24:'
+              className='h-5 px-1 w-[40px] flex items-center border shadow-[inset_0px_0px_12px_rgba(0,0,0,0.25)] rounded-full relative cursor-pointer transition duration-200 bg-neutral-900 border-neutral-500'
+            >
+              <div
+                className='h-[20px] block rounded-full bg-white shadow-md z-10 transition duration-200'
+                style={{
+                  height: '12px',
+                  width: '12px',
+                  transform: `translateX(${timeFrame === 'monthly' ? '-2px' : '20px'}) translateZ(0px)`,
+                }}
+              ></div>
+              <input
+                className='hidden'
+                id=':r24:'
+                type='checkbox'
+                checked={timeFrame === 'yearly'}
+                onChange={handleToggle}
+              ></input>
+            </label>
+            <p
+              className={`text-sm ${timeFrame === 'monthly' ? 'text-neutral-400' : 'text-neutral-100'} transition duration-200`}
+            >
+              Yearly (save 45%)
+            </p>
+          </form>
+        </div>
+        <div>
+          <h3>Free</h3>
+          <h4>
+            {price.free} / {timeFrame}
+          </h4>
+          <h5></h5>
+          <ul>
+            <li></li>
+          </ul>
+          <button>Get Started</button>
+        </div>
+        <div>
+          <h3>Personal</h3>
+          <h4>
+            {price.personal} / {timeFrame}
+          </h4>
+          <h5></h5>
+          <ul>
+            <li></li>
+          </ul>
+          <button>Get Started</button>
+          {timeFrame === 'yearly' ? (
+            <>That&apos;s {discount.personal} per month!</>
+          ) : null}
+        </div>
+        <div>
+          <h3>Family</h3>
+          <h4>
+            {price.family} / {timeFrame}
+          </h4>
+          <h5></h5>
+          <ul>
+            <li></li>
+          </ul>
+          <button>Get Started</button>
+          {timeFrame === 'yearly' ? (
+            <>That&apos;s {discount.family} per month!</>
+          ) : null}
+        </div>
       </div>
     </section>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
 import type { Config } from 'tailwindcss';
+const svgToDataUri = require('mini-svg-data-uri');
+
 const {
   default: flattenColorPalette,
 } = require('tailwindcss/lib/util/flattenColorPalette');
@@ -33,7 +35,21 @@ const config: Config = {
       },
     },
   },
-  plugins: [addVariablesForColors],
+  plugins: [
+    addVariablesForColors,
+    function ({ matchUtilities, theme }: any) {
+      matchUtilities(
+        {
+          'bg-dot': (value: any) => ({
+            backgroundImage: `url("${svgToDataUri(
+              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
+            )}")`,
+          }),
+        },
+        { values: flattenColorPalette(theme('backgroundColor')), type: 'color' }
+      );
+    },
+  ],
 };
 
 function addVariablesForColors({ addBase, theme }: any) {
